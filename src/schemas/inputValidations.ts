64a31c02d3c605015db6293e5b3bc7304527dc50
kosuke <jobsuke05@gmail.com>
2026-03-03 09:17:47 +0900

    commit after updating zod validations

--- src/schemas/inputValidations.ts
+++ src/schemas/inputValidations.ts
@@ -1,52 +1,39 @@
 import * as z from "zod";
 
 // タスク名
-const taskNameSchema = z.object({
+export const schemas = z.object({
+  // タスク名
   taskName:
     z.string()
     .min(2, {
       message: "2文字以上入力して下さい。"
-    })
-});
+    }),
 
-// タスク説明
-const taskDescriptionSchema = z.object({
+  // タスク説明
   taskDescription:
     z.string()
-    .optional()
-});
+    .optional(),
 
-// タスク進捗
-const taskStatusSchema = z.object({
+  // タスク進捗
   taskStatus:
     z.enum(["not-started", "in-progress", "done"])
     .or(z.literal(""))
     .refine(v => v !== "", {
       message: "いずれかを選択して下さい"
-    })
-});
+    }),
 
-// 優先度
-const taskPrioritySchema = z.object({
+  // タスク優先度
   taskPriority:
     z.enum(["low", "middle", "high"])
     .or(z.literal(""))
     .refine(v => v !== "", {
       message: "いずれかを選択して下さい"
-    })
-});
+    }),
 
-// 期限
-const taskDueDateSchema = z.object({
+  // 期限
   dueDate:
     z.string()
     .refine(v => v !== "", {
       message: "提出期限を入力して下さい"
     })
 });
-
-export const schema = taskNameSchema
-.and(taskDescriptionSchema)
-.and(taskStatusSchema)
-.and(taskPrioritySchema)
-.and(taskDueDateSchema);
