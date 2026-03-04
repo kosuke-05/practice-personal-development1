bcfbf3eacfc6772bce8cfb723a4ef2097ff8190a
kosuke <jobsuke05@gmail.com>
2026-03-04 09:39:20 +0900

    登録処理に関する実装

---
+++ src/mocks/handlers/msw.ts
@@ -0,0 +1,21 @@
+import { InputTask } from "@/components/createTask";
+import { HttpResponse, http } from "msw"
+
+// データ配列
+let tasks = [];
+
+export const handlers = [
+  http.post("/api/tasks", async ({ request }) => {
+    // JSオブジェクトに変換
+    const body = (await request.json()) as InputTask;
+
+    // id番号を付与
+    const newTask = { id: crypto.randomUUID(), ...body };
+
+    // idを付与したデータをデータ配列に追加
+    tasks.push(newTask);
+
+    // JSオブジェクトに変換してreturn
+    return HttpResponse.json(newTask, { status: 201 });
+  })
+]
