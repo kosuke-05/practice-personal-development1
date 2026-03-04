ced696448ea143c7e61eb3d3390eec90c7276be7
kosuke <jobsuke05@gmail.com>
2026-03-04 11:35:32 +0900

    取得処理に関する実装

---
+++ src/components/table.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { getHooks } from "@/hooks/getHooks"
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+
+/**
+ * データ一覧をテーブルで描画
+ * ①hooks層を読んでデータを取得
+ * ②テーブル上で取得データを展開
+ */
+export const TableComponent = () => {
+  // hooksから取得
+  const { data } = getHooks();
+
+  return (
+    <>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>業務名</TableCell>
+            <TableCell>業務説明</TableCell>
+            <TableCell>進捗度</TableCell>
+            <TableCell>優先度</TableCell>
+            <TableCell>期限</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.map((item) => (
+            <TableRow key={item.taskName}>
+              <TableCell>{item.taskName}</TableCell>
+              <TableCell>{item.taskDescription}</TableCell>
+              <TableCell>{item.taskStatus}</TableCell>
+              <TableCell>{item.taskPriority}</TableCell>
+              <TableCell>{item.dueDate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  )
+}
